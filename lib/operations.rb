58f1e9885594cc23e28e2ee7ba2bb254a5a7cb21
Rose Seraphin <roseseraphin@yahoo.com>
2019-10-19 00:56:00 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,36 @@
-def unsafe?(speed)
 
+#require 'pry' 
+def unsafe?(speed) # unsafe? takes argument of speed and return true if speed unsafe
+ 
+
+ 
+ if speed > 60
+    return true
+    
+ elsif speed < 40
+  return true 
+    
+  else
+return false 
+ 
+  #unsafe takes an argument of speed and return true if speed is below 40 or above 60
+
+
+end 
 end
-
 
 
 def not_safe?(speed)
+  speed > 60 || speed < 40 ? true : false
+  
 	
 end
 	
 
+# 
 
+ #speed => 60 || speed < 40
+
+# age = 1
+ 
+# age < 2 ? "baby" : "not a baby"
